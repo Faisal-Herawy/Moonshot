4cfe17d5552fc9632faceccb47cf886088816ae9
Faisal-Herawy <faisalherawy@icloud.com>
2024-01-03 19:43:10 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Faisal Herawy on 21/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
